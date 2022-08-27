232a3e0fe890c09975a21f1aa9c2a00b7a9b8705
Abdelrahman Hedia <66520407+bodyhedia44@users.noreply.github.com>
2022-08-27 13:48:51 +0200

    third

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.udacity.shoestore
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import com.udacity.shoestore.databinding.FragmentHomeBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
         Timber.plant(Timber.DebugTree())
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.frag_host) as NavHostFragment
